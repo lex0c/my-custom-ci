a34157606e810f39ff9b5e0ab5ec0f1ccb8e49bf
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-28 22:38:56 -0200

    Refactored class

--- application/libraries/Hash.php
+++ application/libraries/Hash.php
@@ -1,30 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * Hash Generator
- * @link https://github.com/lleocastro/cosmosphp/
- * @license https://github.com/lleocastro/cosmosphp/blob/master/LICENSE
- * @author Léo Castro <leonardo_carvalho@outlook.com>
- * @package \Cosmos\Security\Encryption
- */
 class Hash
 {
     /**
      * Encryption prefix
-     * @see http://www.php.net/security/crypt_blowfish.php
      * @var string
      */
     protected $prefix = '2a';
+
     /**
      * Salt [MTc2MzMxNDQ4NTdmZDg4Yz]
-     * @see http://www.php.net/security/crypt_blowfish.php
      * @var string
      */
     protected $salt = '';
+
     /**
      * Cost default '8' [4 <> 31]
-     * @see http://www.php.net/security/crypt_blowfish.php
      * @var int
      */
     protected $cost = 8;
@@ -36,13 +28,14 @@ class Hash
      * @param string $salt
      * @param int    $cost
      */
-    public function __construct(string $prefix = '', string $salt = '', int $cost = 8)
+    public function __construct($prefix = '', $salt = '', $cost = 8)
     {
-        $prefix = trim($prefix); $salt = trim($salt);
+        $prefix = trim((string) $prefix); $salt = trim((string) $salt); $cost = (int) $cost;
         $this->prefix = ($prefix == '') ? $this->prefix = '2a' : $this->prefix = $prefix;
         $this->salt = ($salt   == '') ? $this->salt = $this->generate_hash() : $this->salt = $salt;
         $this->cost = ($cost < 4 || $cost > 31) ? $this->cost = '8' : $this->cost = $cost;
     }
+
     /**
      * Encrypt Generator.
      *
@@ -50,11 +43,12 @@ class Hash
      *
      * @return string
      */
-    public function generate(string $value):string
+    public function generate($value)
     {
         return str_replace('=', '', strrev(
-            $this->inverse(crypt(strrev($value), $this->generate_hash()))));
+            $this->inverse(crypt(strrev((string) $value), $this->generate_hash()))));
     }
+
     /**
      * Compare hashes.
      *
@@ -63,30 +57,32 @@ class Hash
      *
      * @return bool
      */
-    public function is_equals(string $value, string $hash):bool
+    public function is_equals($value, $hash)
     {
-        $v = strrev($value);
-        $h = $this->reverse(strrev($hash));
+        $v = strrev((string) $value);
+        $h = $this->reverse(strrev((string) $hash));
         if (crypt($v, $h) === $h) {
             return true;
         }
         return false;
     }
+
     /**
      * Generate a random salt.
      *
      * @return string
      */
-    protected function generate_salt():string
+    protected function generate_salt()
     {
         return substr(base64_encode(uniqid(mt_rand(), true)), 0, 22);
     }
+
     /**
      * Build a hash string for crypt.
      *
      * @return string
      */
-    protected function generate_hash():string
+    protected function generate_hash()
     {
         return sprintf('$%s$%02d$%s$', $this->prefix, $this->cost, $this->generate_salt());
     }
@@ -98,13 +94,15 @@ class Hash
      *
      * @return string
      */
-    protected function inverse(string $encryptedData):string
+    protected function inverse($encryptedData)
     {
+        $encryptedData = (string) $encryptedData;
         return base64_encode(strrev(
                 substr($encryptedData, (strlen($encryptedData)/2)-strlen($encryptedData), strlen($encryptedData)).
                 substr($encryptedData, 0, (strlen($encryptedData)/2)-strlen($encryptedData)))
         );
     }
+
     /**
      * Decrypting.
      *
@@ -112,9 +110,9 @@ class Hash
      *
      * @return string
      */
-    protected function reverse(string $encryptedData):string
+    protected function reverse($encryptedData)
     {
-        $encryptedData = base64_decode($encryptedData);
+        $encryptedData = base64_decode((string) $encryptedData);
         return strrev(
             substr($encryptedData, (strlen($encryptedData)/2)-strlen($encryptedData),strlen($encryptedData)).
             substr($encryptedData, 0, (strlen($encryptedData)/2)-strlen($encryptedData))
@@ -130,6 +128,7 @@ class Hash
     {
         return $this->prefix;
     }
+
     /**
      * Set the encrypt prefix.
      *
@@ -137,11 +136,12 @@ class Hash
      *
      * @return Hash
      */
-    public function set_prefix(string $prefix):Hash
+    public function set_prefix($prefix)
     {
-        $this->prefix = trim($prefix);
+        $this->prefix = trim((string) $prefix);
         return $this;
     }
+
     /**
      * Returns the encrypt salt.
      *
@@ -151,6 +151,7 @@ class Hash
     {
         return $this->salt;
     }
+
     /**
      * Set the encrypt salt.
      *
@@ -158,9 +159,9 @@ class Hash
      *
      * @return Hash
      */
-    public function set_salt(string $salt):Hash
+    public function set_salt($salt)
     {
-        $salt = trim($salt);
+        $salt = trim((string) $salt);
         if (strlen($salt) == 22) {
             $this->salt = $salt;
         } else {
@@ -168,6 +169,7 @@ class Hash
         }
         return $this;
     }
+
     /**
      * Returns the encrypt cost.
      *
@@ -177,6 +179,7 @@ class Hash
     {
         return $this->cost;
     }
+
     /**
      * Set the encrypt cost.
      *
@@ -184,8 +187,9 @@ class Hash
      *
      * @return Hash
      */
-    public function set_cost(int $cost):Hash
+    public function set_cost($cost)
     {
+        $cost = (int) $cost;
         if (($cost > 3) && ($cost < 32)) {
             $this->cost = $cost;
         } else {
