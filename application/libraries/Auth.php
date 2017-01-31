43112c26b81c28c5ab01a5fa185b041f0cecc615
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-30 22:58:17 -0200

    Refactoring and optimizing

--- application/libraries/Auth.php
+++ application/libraries/Auth.php
@@ -13,39 +13,39 @@ class Auth
     /**
      *
      */
+    protected $errors = null;
+
+    /**
+     *
+     */
     public function __construct()
     {
         $this->CI =& get_instance();
-        $this->CI->load->model('auth/Auth_model', 'authModel');
+        $this->CI->load->model('auth/Auth_model', 'auth_model');
+        $this->CI->load->library('form_validation');
         $this->CI->load->library('Hash', 'hash');
     }
 
     /**
      * User authentication and session data creation.
      *
-     * @param array  $authData
-     * @param string $redirect
+     * @param array $auth_data
      *
-     * @return mixed
+     * @return Auth
      */
-    public function authenticate(array $authData, $redirect)
+    public function authenticate(array $auth_data)
     {
-        if(!$this->is_authenticated()) {
-            $authData = array_map('htmlentities', $authData);
-            $authData = array_map('trim', $authData);
+        $auth_data = array_map('htmlentities', $auth_data);
+        $this->CI->form_validation->set_rules('email', 'e-mail', 'trim|required|valid_email');
+        $this->CI->form_validation->set_rules('password', 'password', 'required|min_length[6]');
 
-            $user = $this->CI->authModel->get_user($authData['email']);
+        if ($this->CI->form_validation->run() !== false) {
+            $user = $this->CI->auth_model->get_user($auth_data['email']);
 
-            if($user == null) {
-                return $this->CI->load->view('auth/login', array(
-                    'errors' => ['Incorrect email or does not exist.'],
-                    'email_fail' => 'Incorrect email or does not exist.'
-                ));
-            } elseif(!$this->CI->hash->is_equals($authData['password'], $user->password)) {
-                return $this->CI->load->view('auth/login', array(
-                    'errors' => ['Wrong password!'],
-                    'password_fail' => 'Wrong password!'
-                ));
+            if(($user == null)
+                || (!$this->CI->hash->is_equals($auth_data['password'], $user->password))) {
+                $this->errors = 'Invalid credentials';
+                return $this;
             }
 
             $addr  = str_ireplace('.', '', filter_input(INPUT_SERVER, 'REMOTE_ADDR'));
@@ -53,28 +53,30 @@ class Auth
 
             $auth_token = $addr . date("Ymd") . $agent . $user->id;
             $auth_token = $this->CI->hash->generate($auth_token);
-            $this->CI->authModel->set_token($user->id, $auth_token);
+            $this->CI->auth_model->set_token($user->id, $auth_token);
 
             $this->CI->session->set_userdata([
                 'auth_user_id'       => $user->id,
                 'auth_user_name'     => $user->name,
                 'auth_user_lastname' => $user->lastname,
-                'auth_user_email'    => $authData['email'],
+                'auth_user_email'    => $auth_data['email'],
                 'auth_user_status'   => $this->CI->hash->generate('isok'),
                 'auth_user_token'    => $this->CI->hash->generate($agent . $addr . 'ramm4')
             ]);
 
+            return null;
         }
 
-        return redirect($redirect, 200);
+        $this->errors = validation_errors();
+        return $this;
     }
 
     /**
      * Checks whether the guest is authenticated.
      *
-     * @return boolean
+     * @return bool
      */
-    public function is_authenticated()
+    public function can()
     {
         if(($this->CI->session->has_userdata('auth_user_status') != null)
             && ($this->CI->hash->is_equals('isok', $this->CI->session->userdata('auth_user_status')))) {
@@ -92,6 +94,14 @@ class Auth
     }
 
     /**
+     *
+     */
+    public function cannot()
+    {
+        return (!$this->can()) ? true : false;
+    }
+
+    /**
      * Redirect client if it tries to access a route that needs
      * authentication.
      *
@@ -101,11 +111,11 @@ class Auth
      *
      * @return void
      */
-    public function who_see($access, $redirect = '/login', $excepts = [])
+    public function who_see($access, $redirect = 'login', $excepts = [])
     {
         switch($access) {
             case 'auth':
-                if(!$this->is_authenticated()) {
+                if(!$this->can()) {
                     redirect($redirect);
                 }
                 break;
@@ -117,14 +127,39 @@ class Auth
         }
     }
 
+    /**
+     *
+     */
+    public function is_authenticated($redirect_to = 'home')
+    {
+        if ($this->can()) {
+//            if (!empty($excepts)) {
+//                foreach ($excepts as $except) {
+//                    if (($except instanceof $this) && (method_exists($this, $except))) {
+//                        //
+//                    } else {
+//                        return redirect($redirect_to);
+//                    }
+//                }
+//            }
+
+            return redirect($redirect_to);
+        }
+    }
+
+    /**
+     *
+     */
     protected function token_verify()
     {}
 
-
+    /**
+     *
+     */
     public function logout($redirect = '/')
     {
         $this->CI->session->sess_destroy();
-        if(($this->CI->session->has_userdata('auth_user_status') != null)) {
+        if(($this->CI->session->has_userdata('auth_user_status') !== null)) {
             $this->CI->session->unset_userdata([
                 'auth_user_id',
                 'auth_user_name',
@@ -140,12 +175,61 @@ class Auth
         redirect($redirect);
     }
 
-    public function register($name, $email, $password)
-    {}
+    /**
+     *
+     */
+    public function register(array $register_data)
+    {
+        $register_data = array_map('htmlentities', $register_data);
+        $this->CI->form_validation->set_rules('name', 'name', 'trim|required|min_length[3]|max_length[50]');
+        $this->CI->form_validation->set_rules('lastname', 'lastname', 'trim|required|min_length[3]|max_length[50]');
+        $this->CI->form_validation->set_rules('email', 'e-mail', 'trim|required|valid_email|max_length[150]');
+        $this->CI->form_validation->set_rules('password', 'password', 'required|min_length[6]');
+        $this->CI->form_validation->set_rules('password_confirm', 'password', 'required|matches[password]');
 
+        if ($this->CI->form_validation->run() !== false) {
+
+            $addr  = str_ireplace('.', '', filter_input(INPUT_SERVER, 'REMOTE_ADDR'));
+            $agent = filter_input(INPUT_SERVER, 'HTTP_USER_AGENT');
+
+            $auth_token = $addr . date("Ymd") . $agent . $user->id;
+            $auth_token = $this->CI->hash->generate($auth_token);
+
+            $status = $this->CI->auth_model->set_user([
+                'name' => $register_data['name'],
+                'lastname' => $register_data['lastname'],
+                'email'    => $register_data['email'],
+                'password' => $this->CI->hash->generate($register_data['password']),
+                'remember_token' => $auth_token
+            ]);
+
+            if ($status) {
+                return null;
+            }
+        }
+
+        $this->errors = validation_errors();
+        return $this;
+    }
+
+    /**
+     *
+     */
     public function get_user_data()
     {
         return $this->CI->session->all_userdata();
+    }
+
+    /**
+     *
+     */
+    public function errors()
+    {
+        $errors = $this->errors;
+        $errors = trim($errors);
+        $errors = str_ireplace('<p>', '', $errors);
+        $errors = str_ireplace('</p>', '', $errors);
+        return array_filter(explode('.', $errors));
     }
 
 }
