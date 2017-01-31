f812f097e29ca09f53e9cd949f26e716c9148c51
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-30 22:54:46 -0200

    Removed unnecessary library load

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array(
-    'database', 'session', 'form_validation', 'auth'
+    'database', 'session', 'auth'
 );
 
 /*
@@ -92,7 +92,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 $autoload['helper'] = array(
-    'security', 'url', 'form', 'globals'
+    'security', 'url', 'globals'
 );
 
 /*
--- template/views/auth/sign-in.php
+++ template/views/auth/sign-in.php
@@ -2,11 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <meta name="description" content=""/>
-    <meta name="author" content=""/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="robots" content="index, follow"/>
+    <meta name="robots" content="noindex, nofollow"/>
 
     <link rel="stylesheet" href="<?php echo asset('css/app.min.css') ?>"/>
     <link rel="stylesheet" href="<?php echo asset('css/toastr.min.css') ?>"/>
@@ -16,29 +14,16 @@
 
     <?php echo ie_support_field() ?>
 
-    <!-- JSON LD -->
-    <script type="application/ld+json">
-        {
-            "@context": "https://schema.org"
-        }
-    </script>
-
     <title>Login</title>
 </head>
 <body>
-    <?php if(isset($errors) && $errors != null):
-        foreach($errors as $error): ?>
-            <script>
-                toastr.warning("<?= trim($error) ?>", 'Dados incorretos!');
-            </script>
-        <?php endforeach;
-    endif; ?>
-
+    <?php require_once (dirname(__DIR__) . '/layouts/includes/show-errors.php'); ?>
     <?php require_once (dirname(__DIR__) . '/layouts/includes/navbar.php'); ?>
+
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
+                <div class="panel panel-primary">
                     <div class="panel-heading">Sign in to access the authenticated area</div>
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="<?php echo route('login/auth') ?>" accept-charset="utf-8">
@@ -53,16 +38,6 @@
                                 <label for="password" class="col-md-4 control-label">Password</label>
                                 <div class="col-md-6">
                                     <input id="password" name="password" type="password" class="form-control" placeholder="Strong password, please!">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" value="" checked/> Remember Me
-                                        </label>
-                                    </div>
                                 </div>
                             </div>
 
@@ -85,4 +60,5 @@
     </div>
 </body>
     <script src="<?php echo asset('js/app.min.js') ?>"></script>
+    <script src="<?php echo asset('js/bootstrap.min.js') ?>"></script>
 </html>
