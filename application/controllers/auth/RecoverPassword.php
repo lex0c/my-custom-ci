179bffa41de3fd0f4626ba84a77f348190863464
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-31 20:20:56 -0200

    Basics updates in structure

--- application/controllers/auth/RecoverPassword.php
+++ application/controllers/auth/RecoverPassword.php
@@ -3,10 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class RecoverPassword extends CI_Controller
 {
+    /**
+     * Where to redirect users after recover.
+     *
+     * @var string
+     */
+    protected $redirect_to = 'password/reset';
+
+    /**
+     * Create a new controller instance.
+     */
     public function __construct()
     {
         parent::__construct();
+        $this->auth->who_see('noauth');
     }
 
+    //
 
 }
