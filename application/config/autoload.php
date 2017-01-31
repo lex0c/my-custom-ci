6597155626a13be163ce42a3a18d9d6bef4dc3f4
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-31 20:54:49 -0200

    Facilities..

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,9 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array(
-    'database', 'session', 'auth'
-);
+$autoload['libraries'] = explode(',', getenv('LIBRARIES'));
 
 /*
 | -------------------------------------------------------------------
@@ -91,9 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array(
-    'security', 'url', 'globals'
-);
+$autoload['helper'] = explode(',', getenv('HELPERS'));
 
 /*
 | -------------------------------------------------------------------
